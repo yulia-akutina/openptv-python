e69c35b52b42e024544a14f105b3c0997358b4f1
Alex Liberzon <alex.liberzon@gmail.com>
2012-07-09 21:11:55 +0300

    added  an option to change the sequence numbers in the batch run, without GUI

--- pyptv_gui/pyptv_batch.py
+++ pyptv_gui/pyptv_batch.py
@@ -2,7 +2,10 @@
 Python/Enthought Traits GUI/Numpy/Chaco
 
 Example:
->> python pyptv_batch.py experiments/exp1 
+>> python pyptv_batch.py experiments/exp1 10001 10022
+
+where 10001 is the first file in sequence and 10022 is the last one
+the present "active" parameters are kept intact except the sequence 
 
 
 """
@@ -15,7 +18,6 @@ import numpy as np
 
 # project specific inputs
 import parameters as par
-import ptv1 as ptv
 import general
 
 
@@ -25,8 +27,8 @@ cwd = os.getcwd()
 
 # import pdb; pdb.set_trace()
 
-if len(sys.argv) < 2:
-	error("Wrong number of inputs, usage: python pyptv_batch.py experiments/exp1")
+if len(sys.argv) < 4:
+	print("Wrong number of inputs, usage: python pyptv_batch.py experiments/exp1 seq_first seq_last")
 	
 software_path = os.path.split(os.path.abspath(sys.argv[0]))[0]
 print 'software_path=', software_path
@@ -38,6 +40,7 @@ except:
 
 src_path = os.path.join(os.path.split(os.path.abspath(os.getcwd()))[0],'src_c')
 sys.path.append(src_path)
+import ptv1 as ptv
 
 
 
@@ -96,14 +99,14 @@ def sequence_tracking(n_img):
 	ptv.py_trackcorr_finish(step+1)
 	print "tracking without display finished"
 
-# backward tracking
-	ptv.py_trackback_c()
+# backward tracking is 	temporary disabled !!!
+	# ptv.py_trackback_c() 
 
 
 
 
-def run_batch():
-# 	import pdb; pdb.set_trace()
+def run_batch(new_seq_first,new_seq_last):
+#  	import pdb; pdb.set_trace()
 	ptv.py_init_proc_c()
 	ptv.py_start_proc_c() # or ptv.py_init_proc_c()?
 	ptvParams = par.PtvParams(path = par.temp_path)
@@ -111,14 +114,22 @@ def run_batch():
 	(n_img, img_name, img_cal, hp_flag, allCam_flag, tiff_flag, imx, imy, pix_x, pix_y, chfield, mmp_n1, mmp_n2, mmp_n3, mmp_d) = \
 		(ptvParams.n_img, ptvParams.img_name, ptvParams.img_cal, ptvParams.hp_flag, ptvParams.allCam_flag, ptvParams.tiff_flag, \
 		ptvParams.imx, ptvParams.imy, ptvParams.pix_x, ptvParams.pix_y, ptvParams.chfield, ptvParams.mmp_n1, ptvParams.mmp_n2, ptvParams.mmp_n3, ptvParams.mmp_d)
-#
-
+# read the sequence parameters
+	sequenceParams = par.SequenceParams(n_img, path = par.temp_path)
+	sequenceParams.read()
+	(base_name, seq_first, seq_last) = (sequenceParams.base_name, sequenceParams.first, sequenceParams.last)
+# write the new sequence parameters
+	par.SequenceParams(n_img, base_name,\
+									   new_seq_first, new_seq_last, path = par.temp_path).write()
 	sequence_tracking(n_img)
 
 
 if __name__ == '__main__':
+
 	try:
-		run_batch()
+		seq_first = sys.argv[2]
+		seq_last = sys.argv[3]
+		run_batch(eval(seq_first),eval(seq_last))
 	except:
 		print("something wrong with the software or folder")
 		general.printException()
