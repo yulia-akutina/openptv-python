54d935669265ed18e0a8830a3fea694814c73f65
Yosef Meller <mellerf@netvision.net.il>
2012-07-14 15:19:06 +0200

    Boilerplate for testing the C files.

---
+++ tests_c/check_ptv.c
@@ -0,0 +1,13 @@
+
+/*  Unit-test suit for the C core of PyPTV. Uses the Check framework:
+    http://check.sourceforge.net/
+    
+    To run it, type "make check" when in the top C directory, src_c/
+*/
+
+#include <stdlib.h>
+
+int main(void) {
+    return EXIT_SUCCESS;
+}
+
