55441dbc648a6352b731e98bd37d16acbbe6c78d
Yosef Meller <mellerf@netvision.net.il>
2012-07-16 12:09:31 +0300

    Test read_targets()

--- tests_c/check_ptv.c
+++ tests_c/check_ptv.c
@@ -42,12 +42,43 @@ START_TEST(test_allocate_tracking_structs)
 }
 END_TEST
 
+/* Check that target t1 is equal to target t2, i.e. all their fields are equal.
+*/
+int compare_targets(target *t1, target *t2) {
+    return (\
+        (t1->pnr == t2->pnr) && (t1->x == t2->x) && (t1->y == t2->y) && \
+        (t1->n == t2->n) && (t1->nx == t2->nx) && (t1->ny == t2->ny) && \
+        (t1->sumg == t2->sumg) && (t1->tnr == t2->tnr));
+}
+
+START_TEST(test_read_targets)
+{
+    target tbuf[2]; /* Two targets in the sample target file */
+    target t1 = {0, 1127.0000, 796.0000, 13320, 111, 120, 828903, 1};
+    target t2 = {1, 796.0000, 809.0000, 13108, 113, 116, 658928, 0};
+    
+    char *file_base = "testing_fodder/sample_";
+    int frame_num = 42;
+    int targets_read = 0;
+    
+    targets_read = read_targets(tbuf, file_base, frame_num);
+    fail_unless(targets_read == 2);
+    fail_unless(compare_targets(tbuf, &t1));
+    fail_unless(compare_targets(tbuf + 1, &t2));
+}
+END_TEST
+
 Suite* ptv_suite(void) {
     Suite *s = suite_create ("PTV");
+
     TCase *tc_tw = tcase_create ("Tracking window");
     tcase_add_test(tc_tw, test_allocate_tracking_structs);
     suite_add_tcase (s, tc_tw);
     
+    TCase *tc_trt = tcase_create ("Read targets");
+    tcase_add_test(tc_trt, test_read_targets);
+    suite_add_tcase (s, tc_trt);
+
     return s;
 }
 
