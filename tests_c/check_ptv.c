f61703d67591a0b0a4573947221ca8764f8fe459
Yosef Meller <mellerf@netvision.net.il>
2012-10-18 16:31:27 +0200

    Fix write_ori test to actually test the added_par writing.

--- tests_c/check_ptv.c
+++ tests_c/check_ptv.c
@@ -90,8 +90,8 @@ START_TEST(test_write_ori)
     char add_file[] = "testing_fodder/test.addpar";
     
     write_ori(correct_cal.ext_par, correct_cal.int_par,
-        correct_cal.glass_par, ori_file, NULL);
-    fail_if((cal = read_calibration(ori_file, "", NULL)) == NULL);
+        correct_cal.glass_par, correct_cal.added_par, ori_file, add_file);
+    fail_if((cal = read_calibration(ori_file, add_file, NULL)) == NULL);
     fail_unless(compare_calib(cal, &correct_cal));
     
     remove(ori_file);
