54cb6e95caef722ba8dcba5c2a312303a8e9bcd5
Yosef Meller <mellerf@netvision.net.il>
2012-11-22 15:13:59 +0200

    Regression test for epi_mm()

---
+++ tests_c/check_epi.c
@@ -0,0 +1,56 @@
+/* Unit/regression tests for code in src_c/epi.c */
+
+#include <check.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "../src_c/calibration.h"
+#include "../src_c/typedefs.h"
+
+START_TEST(test_epi_mm)
+{
+    double xin, yin;
+    double xmin, ymin, xmax, ymax;
+    mm_np media_par = {1, 1., {1.49, 0., 0.}, {5., 0., 0.}, 1.33, 1.};
+    Calibration* cal[2];
+    
+    fail_if(chdir("testing_fodder/") != 0);
+    init_proc_c();
+    
+    cal[0] = read_calibration("cal/cam1.tif.ori",
+        "cal/cam1.tif.addpar", NULL);
+    cal[1] = read_calibration("cal/cam2.tif.ori",
+        "cal/cam2.tif.addpar", NULL);
+    
+    xin = 10.;
+    yin = 10.;
+    epi_mm(xin, yin,
+        cal[0]->ext_par, cal[0]->int_par, cal[0]->glass_par,
+        cal[1]->ext_par, cal[1]->int_par, cal[1]->glass_par,
+        media_par, &xmin, &ymin, &xmax, &ymax);
+    
+    fail_unless((abs(xmin - (-9.209233)) < 1e-6) && (abs(ymin - 21.758034) < 1e-6) 
+        && (abs(xmax - 10.067018) < 1e-6) && (abs(ymax - 20.877886) < 1e-6));
+}
+END_TEST
+
+Suite* epi_suite(void) {
+    Suite *s = suite_create ("Epipolar lines");
+
+    TCase *tc = tcase_create ("Line window 3D");
+    tcase_add_test(tc, test_epi_mm);
+    suite_add_tcase (s, tc);
+
+    return s;
+}
+
+int main(void) {
+    int number_failed;
+    Suite *s = epi_suite ();
+    SRunner *sr = srunner_create (s);
+    srunner_run_all (sr, CK_ENV);
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
