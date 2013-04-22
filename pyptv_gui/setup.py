06eba1742f8d7e640edad44850e1e94f27a5bcbf
Yosef Meller <yosefmel@post.tau.ac.il>
2013-04-22 13:21:16 +0300

    Build under MinGW: sort out the include paths and the extern declarations

--- pyptv_gui/setup.py
+++ pyptv_gui/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
-from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Distutils.extension import Extension
 
 import numpy as np
 import os
@@ -15,6 +15,8 @@ else:
 origdir = os.getcwd()
 os.chdir('../src_c')
 
+inc_dirs = [np.get_include()]
+
 ext_mods = [
     Extension("ptv1", ["ptv1.pyx", "segmentation.c", "tools.c",
         "image_processing.c", "trafo.c", "jw_ptv.c", "peakfitting.c", 
@@ -22,11 +24,17 @@ ext_mods = [
         "ray_tracing.c", "imgcoord.c", "lsqadj.c", "orientation.c","sortgrid.c",
         "pointpos.c", "intersect.c", "track.c", "ttools.c", "draw.c",
         "mousefunction.c", "vec_utils.c", "parameters.c", "tracking_run.c"],
-        include_dirs=[np.get_include(),'.'], libraries=['optv'],
-        extra_compile_args=['-O3', '-fno-common']),
-    Extension("tracking_framebuf", ["tracking_framebuf.pyx"], libraries=['optv']),
-    Extension("tracking_run_py", ["tracking_run_py.pyx", "tracking_run_py.pxd", 
-        "tracking_run.c", "parameters.c"], libraries=['optv']) ]
+        include_dirs=inc_dirs, libraries=['optv'], 
+        extra_compile_args=['-O3'],
+	pyrex_include_dirs=['.']),
+    Extension("tracking_framebuf", ["tracking_framebuf.pyx"], 
+    	libraries=['optv'], include_dirs=inc_dirs,
+	pyrex_include_dirs=['.']),
+    Extension("tracking_run_py", ["tracking_run_py.pyx", 
+        "tracking_run.c", "parameters.c"], 
+	libraries=['optv'], include_dirs=inc_dirs,
+	pyrex_include_dirs=['.']) 
+    ]
 
 setup(
     name="ptv1",
